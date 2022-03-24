0c862e967bc72050c1bb5e5d09c309843b90e8a0
jakedoublev <vanvorhisjake@gmail.com>
2022-03-23 18:49:18 -0700

    fixed liveshare bug with the config being lagged

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,17 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.tsx',
-  output: { path: path.re },
+  output: { path: path.resolve(__dirname, 'build') },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
 };
