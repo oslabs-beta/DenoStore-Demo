14dcfb8713326232600dc56d2d9c8027435adead
TX Ho <tingxianho@gmail.com>
2022-03-26 20:07:03 +0000

    Server not running on TX's computer

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,12 @@
 // server and backend running in Deno
+import { Application } from 'https://deno.land/x/oak/mod.ts';
+
+// import { FsFile } from 'https://deno.land/x/oak@v10.5.1/send.ts';
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = 'Hello world';
+});
+
+await app.listen({ port: 8000 });
