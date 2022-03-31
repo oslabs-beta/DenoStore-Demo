2f4cb6223ae5364e09a3a9e88ab7f825022b0187
jakedoublev <vanvorhisjake@gmail.com>
2022-03-31 14:36:32 -0700

    working webpack and deno backend with router

--- server/router.ts
+++ server/router.ts
@@ -1,14 +1,21 @@
 // @ts-ignore
-import { Router, Context, send } from 'https://deno.land/x/oak@v10.2.0/mod.ts';
+import { Router, Context } from 'https://deno.land/x/oak@v10.2.0/mod.ts';
 
 const router = new Router();
 
-// router.get('/', async (ctx: Context) => {
-//   await send(ctx, ctx.request.url.pathname, {
-//     root: `${Deno.cwd()}/src/`,
-//     index: 'index.html',
-//   });
-// });
+// https://github.com/oakserver/oak#:~:text=listen(%7B%20port%3A%208000%20%7D)%3B-,Static,-content
+// random.html is just a different file than the main index.html to test that our router can serve
+// other data than just our front-end index.html upon get requests
+router.get('/', async (ctx: Context, next) => {
+  try {
+    await ctx.send({
+      root: `${Deno.cwd()}/src`,
+      index: 'random.html',
+    });
+  } catch {
+    next();
+  }
+});
 
 router.get('', (ctx: Context) => {
   ctx.response.status = 404;
--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,14 @@
 // server and backend running in Deno
 
 // @ts-ignore
-import { Application, Context } from 'https://deno.land/x/oak@v10.2.0/mod.ts';
+import { Application } from 'https://deno.land/x/oak@v10.2.0/mod.ts';
 // @ts-ignore
-// import router from './router.ts';
+import router from './router.ts';
 
 const app = new Application();
 
-// app.use(router.routes());
-// app.use(router.allowedMethods());
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 const port = 3000;
 
@@ -16,9 +16,9 @@ app.addEventListener('listen', () => {
   console.log(`Listening on localhost:${port}`);
 });
 
-app.use((ctx: Context) => {
-  ctx.response.body = 'server working';
-});
+// app.use((ctx: Context) => {
+//   ctx.response.body = 'server working';
+// });
 
 app.addEventListener('error', (event) => {
   console.error(event.error);
