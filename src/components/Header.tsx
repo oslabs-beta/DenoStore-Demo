6f309af2d5cf50540e653d1f9d8dd9c87f2afdce
Jessica Wachtel <jlwachtel@gmail.com>
2022-04-07 14:43:09 -0400

    css in ts removed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,19 +3,11 @@ import * as React from 'react';
 import { Link} from 'react-scroll';
 import './../styles.css';
 
-const styles: { [key: string]: React.CSSProperties } = {
-    container: {
-     display: 'flex',
-     justifyContent: 'space-around', 
-     listStyle: 'none'
-    },
-  };
-
 const Header: React.FC = () => {
     return (
         <div className="header">     
 
-        <ul style={styles.container} >
+        <ul className="headerBody" >
           <li><Link activeClass="active" to="home" spy={true} smooth={true}>Home</Link></li>
           <li><Link  to="demo" spy={true} smooth={true}>Demo</Link></li>
           <li><Link  to="team" spy={true} smooth={true}>Team</Link></li>
