697e3d5532c51a3dd8630973dfb15a0abcfe1604
jakedoublev <vanvorhisjake@gmail.com>
2022-04-18 19:03:01 -0700

    have not yet styled documentation page

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -11,31 +11,36 @@ const Homepage: React.FC = () => {
   return (
     <div>
       <Header />
-      <h1 className="DenoStore-title">DenoStore</h1>
-      <div id="main-section">
-        <div className="homepage-paragraphs-div">
-          <nav className="homepage-paragraphs" id="documentation-homepage-link">
-            Learn more about how to incorporate DenoStore in your application in
-            our <Link to="/docs">documentation page</Link>.
-          </nav>
-          <p className="homepage-paragraphs">
-            Caching is a necessity at scale, but setting it up for GraphQL can
-            be challenging and time-consuming, even more so in a Deno runtime
-            environment.
-          </p>
+      <div className="homepage-without-header">
+        <h1 className="DenoStore-title">DenoStore</h1>
+        <div id="main-section">
+          <div className="homepage-paragraphs-div">
+            <nav
+              className="homepage-paragraphs"
+              id="documentation-homepage-link"
+            >
+              Learn more about how to incorporate DenoStore in your application
+              in our <Link to="/docs">documentation page</Link>.
+            </nav>
+            <p className="homepage-paragraphs">
+              Caching is a necessity at scale, but setting it up for GraphQL can
+              be challenging and time-consuming, even more so in a Deno runtime
+              environment.
+            </p>
 
-          <p className="homepage-paragraphs">
-            DenoStore makes caching GraphQL queries through a Deno/Oak server
-            quick to set up and easy to configure. Get ready for lightning fast
-            GraphQL.
-          </p>
+            <p className="homepage-paragraphs">
+              DenoStore makes caching GraphQL queries through a Deno/Oak server
+              quick to set up and easy to configure. Get ready for lightning
+              fast GraphQL.
+            </p>
+          </div>
+          <img src={logo} alt="Logo" id="logo" />
+          {/* <div className="graphic">Insert Slideshow Graphic here</div> */}
         </div>
-        <img src={logo} alt="Logo" id="logo" />
-        {/* <div className="graphic">Insert Slideshow Graphic here</div> */}
+        <DemoContainer />
+        <TeamSection />
+        <Footer />
       </div>
-      <DemoContainer />
-      <TeamSection />
-      <Footer />
     </div>
   );
 };
