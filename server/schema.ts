391e2fa4cb9f11658f1521cb27616bd61068afb4
James Kim <james.minjae.97@gmail.com>
2022-04-19 23:04:46 -0400

    added more fields to schema and cleaned up server.ts

--- server/schema.ts
+++ server/schema.ts
@@ -1,54 +1,44 @@
 import { gql } from 'https://deno.land/x/graphql_tag@0.0.1/mod.ts';
 
-
-
 export const resolvers = {
-    Query: {
-       oneRocket: async (           
-           _parent: any,
-           args: any,
-           { denostore }: any,
-           info: any
-       ) => {
-        return await denostore.cache( 
-        { info },
-        async () => {
-            const results = await fetch (
-                `https://api.spacexdata.com/v3/rockets/${args.id}`
-            ).then((res) => res.json());
-            console.log('api call');
-           
-            return results
-          }        
-        );
-       
+  Query: {
+    oneRocket: async (
+      _parent: any,
+      args: any,
+      { denostore }: any,
+      info: any
+    ) => {
+      return await denostore.cache({ info }, async () => {
+        const results = await fetch(
+          `https://api.spacexdata.com/v3/rockets/${args.id}`
+        ).then((res) => res.json());
+        // console.log('api call');
+
+        return results;
+      });
     },
-     manyRockets: async (
-        _parent: any,
-        ags: any,
-        { denostore }: any,
-        info: any 
-     ) => {
-        return await denostore.cache(
-        { info },
-         async () => {
-            const results = await fetch (
-                `https://api.spacexdata.com/v3/rockets`
-            ).then((res) => res.json());
-            console.log('api call');
-            return results
-        }
-    );
-      
-    }
-  }
-}
+    rockets: async (
+      _parent: any,
+      _args: any,
+      { denostore }: any,
+      info: any
+    ) => {
+      return await denostore.cache({ info }, async () => {
+        const results = await fetch(
+          `https://api.spacexdata.com/v3/rockets`
+        ).then((res) => res.json());
+        // console.log('api call');
+        return results;
+      });
+    },
+  },
+};
 
 export const typeDefs = gql`
-type RocketType {
-    id: String
+  type RocketType {
+    id: Int
     active: String
-    stages: String
+    stages: Int
     first_flight: String
     country: String
     wikipedia: String
@@ -56,12 +46,17 @@ type RocketType {
     rocket_id: String
     rocket_name: String
     rocket_type: String
-}
+    height: DimensionType
+    diameter: DimensionType
+  }
 
+  type DimensionType {
+    meters: Float
+    feet: Float
+  }
 
-type Query {
+  type Query {
     oneRocket(id: ID): RocketType
-    manyRockets: [RocketType]!
-}
-`
-
+    rockets: [RocketType]!
+  }
+`;
