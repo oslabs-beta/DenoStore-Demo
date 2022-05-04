e407e976ecb3e40b80c3e714c834a42c0d026320
James Kim <james.minjae.97@gmail.com>
2022-05-03 21:01:17 -0400

    some edits

--- server/routes/denostore.ts
+++ server/routes/denostore.ts
@@ -3,17 +3,24 @@ import { Denostore, connect, parseURL } from '../deps.ts';
 import { typeDefs, resolvers } from './../schema.ts';
 
 // set up redis connection
+const redis = await connect({
+  hostname: 'localhost',
+  port: 6379,
+});
+
+// for docker-compose
 // const redis = await connect({
-//   hostname: 'localhost',
-//   port: 6379,
+//   hostname: String(Deno.env.get('REDIS_HOST')),
+//   port: Deno.env.get('REDIS_PORT'),
 // });
 
 // for heroku deployment
-const redis = await connect(parseURL(String(Deno.env.get('REDIS_URL'))));
+// const redis = await connect(parseURL(String(Deno.env.get('REDIS_URL'))));
 
 //instantiate Denostore
 export const denostore = new Denostore({
   schema: { typeDefs, resolvers },
   usePlayground: true,
   redisClient: redis,
+  defaultEx: 10,
 });
