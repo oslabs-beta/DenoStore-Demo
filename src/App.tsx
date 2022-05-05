9d277d4fb0b8908e6841b6ff9e8ded704126a60d
James Kim <james.minjae.97@gmail.com>
2022-05-05 00:04:16 -0400

    added comment

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ const App: React.FC = () => {
   return (
     <div>
       <BrowserRouter>
+        // router no longer neccessary, but left it for future development
         <Routes>
           <Route path="/" element={<Homepage />} />
         </Routes>
